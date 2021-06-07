85df4c39860cdebd86a5cf85896f48db4ef4924f
ademarjoaquim <ajoaquim@yahoo.com>
2021-06-06 22:27:29 -0300

    waçdermar professor

--- JavaAloGitHub/src/alogithub/Alogithub.java
+++ JavaAloGitHub/src/alogithub/Alogithub.java
@@ -14,7 +14,7 @@ public class Alogithub {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Alo GitHub!!");
-		System.out.println("Prof HAdemar de Souza JOaquimn");
+		System.out.println("Prof Waldemar de Souza JOaquimn");
 		
 
 	}
