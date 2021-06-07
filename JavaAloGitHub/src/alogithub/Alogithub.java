10651adc1eb3c9bb37a30b562d4ac7a5be94a54a
asouzaj <59539408+asouzaj@users.noreply.github.com>
2021-06-06 21:53:49 -0300

    adicione via upload

---
+++ JavaAloGitHub/src/alogithub/Alogithub.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package alogithub;
+
+/**
+ * @author ajoaquim
+ *
+ */
+public class Alogithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Alo GitHub!!");
+
+	}
+
+}
